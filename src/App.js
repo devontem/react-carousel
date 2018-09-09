aec34e59817412916944b2eac0b9a65e6b7f517f
Devonte MacGlashan <devontemacglashan@macbook-pro-10.attlocal.net>
2018-09-08 20:12:31 -0700

    added slide module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
-import Header from './components/Header/Header'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header title={"KING KONG"} />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
